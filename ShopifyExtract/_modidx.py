bc8f352f11988da8e8cb7ac29fb150087794d593
mozzieja <jasonmossnz@gmail.com>
2025-01-10 13:05:19 +1300

    exported custoemr import modules to .py files

--- ShopifyExtract/_modidx.py
+++ ShopifyExtract/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://mozzieja.github.io',
                 'git_url': 'https://github.com/mozzieja/ShopifyExtract',
                 'lib_path': 'ShopifyExtract'},
-  'syms': {'ShopifyExtract.core': {'ShopifyExtract.core.foo': ('core.html#foo', 'ShopifyExtract/core.py')}}}
+  'syms': { 'ShopifyExtract.core': { 'ShopifyExtract.core.create_schema': ('core.html#create_schema', 'ShopifyExtract/core.py'),
+                                     'ShopifyExtract.core.fetch_all_customers': ('core.html#fetch_all_customers', 'ShopifyExtract/core.py'),
+                                     'ShopifyExtract.core.fetch_new_or_updated_customers': ( 'core.html#fetch_new_or_updated_customers',
+                                                                                             'ShopifyExtract/core.py'),
+                                     'ShopifyExtract.core.store_customers_in_db': ( 'core.html#store_customers_in_db',
+                                                                                    'ShopifyExtract/core.py')}}}
